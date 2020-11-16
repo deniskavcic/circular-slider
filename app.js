fcec27bea9a9e32059ba412e952ca1ccba982875
Denis Kavčič <denis.kavcic@sberbank.si>
2020-11-16 15:26:38 +0100

    slider path parametrization

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class Slider {
+class CircularSlider {
 
     sliders = [];
 
@@ -9,8 +9,9 @@ class Slider {
 
         this.sliderWidthHeight = 600;   
         this.circleParticle = {
-            length: 20,
-            spacing: 1
+            length: 15,
+            width: 25,
+            color: "#bfbfbf"
         }                                  
     }
 
@@ -35,15 +36,43 @@ class Slider {
         svg.appendChild(sliderGroup);
 
         //calculations
-        const circumference = this.sliders[this.sliders.length - 1].radius * 2 * Math.PI;
+        const circumference = this.sliders[this.sliders.length - 1].radius * 2 * Math.PI; 
+        const numOfParticles = Math.floor((circumference / this.circleParticle.length) * 0.8);
+        const totalSpacing = circumference - numOfParticles * this.circleParticle.length;
+        let circleParticleSpacing = totalSpacing / numOfParticles;
 
+        //draw
+        this.drawSliderPath(sliderGroup, circleParticleSpacing);
+    }
+
+    drawSliderPath(sliderGroup, circleParticleSpacing) {
         //draw slider path
         const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        path.setAttribute('d', "M 500 299 A 200 200 0 1 0 500 300 z");
-        path.style.stroke = this.sliders[this.sliders.length - 1].color;
-        path.style.strokeWidth = 10;
+        path.setAttribute('d', this.pathAttribute(this.sliderWidthHeight/2, this.sliderWidthHeight/2, this.sliders[this.sliders.length - 1].radius, 360));
+        path.style.stroke = this.circleParticle.color;
+        path.style.strokeWidth = this.circleParticle.width;
         path.style.fill = 'none';
-        path.setAttribute('stroke-dasharray', this.circleParticle.length + ' ' + this.circleParticle.spacing);
+        path.setAttribute('stroke-dasharray', this.circleParticle.length + ' ' + circleParticleSpacing);
         sliderGroup.appendChild(path);
     }
+
+    pathAttribute (centerX, centerY, radius, angle) {
+        angle = angle === 360 ? 359 : angle;
+        let arc = angle <= 180 ? '0' : '1';
+        let start = this.polarToCartesian(centerX, centerY, radius, angle);
+        let end = this.polarToCartesian(centerX, centerY, radius, 0);
+        let path = ['M', start.x, start.y, 'A', radius, radius, 0, arc, 0, end.x, end.y].join(' ');
+
+        if (angle === 359) path = path + ' Z';
+
+        return path;
+    }
+
+    polarToCartesian (centerX, centerY, radius, angleDeg) {
+        const angleRad = angleDeg * Math.PI / 180;
+        const x = centerX + (radius * Math.cos(angleRad));
+        const y = centerY + (radius * Math.sin(angleRad));
+        return {x, y};
+    }
+
 }
